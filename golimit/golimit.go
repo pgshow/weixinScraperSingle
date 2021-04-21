35208cd7ca13fd4f2d92080cc00cebb71af35c6c
pgshow <pgshow@gmail.com>
2021-04-21 16:54:53 +0800

    add golimit.go

---
+++ golimit/golimit.go
@@ -0,0 +1,18 @@
+package golimit
+
+// 限制并发数
+type GoLimit struct {
+	ch chan int
+}
+
+func NewGoLimit(max int) *GoLimit {
+	return &GoLimit{ch: make(chan int, max)}
+}
+
+func (g *GoLimit) Add() {
+	g.ch <- 1
+}
+
+func (g *GoLimit) Done() {
+	<-g.ch
+}
